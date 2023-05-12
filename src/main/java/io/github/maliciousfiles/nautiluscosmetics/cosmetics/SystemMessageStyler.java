729be4a394e7ec32c496591d5901aff843035270
Asa Shepard <ashepard@cyc.com>
2023-05-11 23:57:20 -0500

    recent chat messages shown on join

--- src/main/java/io/github/maliciousfiles/nautiluscosmetics/cosmetics/SystemMessageStyler.java
+++ src/main/java/io/github/maliciousfiles/nautiluscosmetics/cosmetics/SystemMessageStyler.java
@@ -1,8 +1,6 @@
 package io.github.maliciousfiles.nautiluscosmetics.cosmetics;
 
-import io.github.maliciousfiles.nautiluscosmetics.NautilusCosmetics;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.ComponentBuilder;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.TranslatableComponent;
 import org.bukkit.Bukkit;
@@ -16,11 +14,12 @@ import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SystemMessageStyler implements Listener {
+
+    public ArrayList<TextComponent> runningMessages = new ArrayList<>();
 
     private static TranslatableComponent styleMessage(TranslatableComponent message) {
         List<Component> args = new ArrayList<>(message.args());
@@ -42,16 +41,30 @@ public class SystemMessageStyler implements Listener {
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
         e.deathMessage(styleMessage((TranslatableComponent) e.deathMessage()));
+        TextComponent tc = Component.text("").append(e.getPlayer().displayName())
+                .append(Component.text(e.getDeathMessage().substring(e.getPlayer().getName().length())));
+        addToRunning(tc);
     }
 
     @EventHandler(priority=EventPriority.HIGHEST)
     public void onJoin(PlayerJoinEvent e) {
         e.joinMessage(styleMessage((TranslatableComponent) e.joinMessage()));
+        TextComponent tc = Component.text(ChatColor.GREEN + "Join " + ChatColor.WHITE + "| ").append(e.getPlayer().displayName());
+        addToRunning(tc);
+    }
+
+    @EventHandler
+    public void onJoinRunning(PlayerJoinEvent e) {
+        for(TextComponent c : runningMessages) {
+            e.getPlayer().sendMessage(c);
+        }
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent e) {
         e.quitMessage(styleMessage((TranslatableComponent) e.quitMessage()));
+        TextComponent tc = Component.text(ChatColor.DARK_RED + "Leave " + ChatColor.WHITE + "| ").append(e.getPlayer().displayName());
+        addToRunning(tc);
     }
 
     @EventHandler
@@ -64,6 +77,13 @@ public class SystemMessageStyler implements Listener {
             p.sendMessage(textComponent);
         }
         Bukkit.getLogger().info(e.getPlayer().getName() + " » " + e.getMessage());
+
+        addToRunning(textComponent);
+    }
+
+    private void addToRunning(TextComponent c) {
+        runningMessages.add(c);
+        if(runningMessages.size() > 50) runningMessages.remove(0);
     }
 
 }
